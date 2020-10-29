84f5c6f3ee9c7b14012a4a24af0678cc61621ef2
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2020-10-29 20:53:49 +0300

    + scripts

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,8 +2,35 @@
 
 public class Ball : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public Platform platform;
+    public float speed;
+    bool isStarted;
+
+    void Update()
+    {
+        if (isStarted)
+        {
+
+        }
+        else
+        {
+            Vector3 platformPosotion = platform.transform.position;
+            Vector3 ballNewPosition = new Vector3(platformPosotion.x, platformPosotion.y + 0.65f, 0);
+            transform.position = ballNewPosition;
+
+            if (Input.GetMouseButtonDown(0))
+                StartBall();
+        }
+    }
+    void StartBall()
+    {
+        Vector2 force = new Vector2(1, 1);
+        rb.AddForce(force * speed);
+        isStarted = true;
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
     }
 }
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 mousePixelPosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePixelPosition);
+
+        Vector3 padNewPosition = mouseWorldPosition;
+
+        padNewPosition.z = 0;
+        padNewPosition.y = -4.3f;
+
+        transform.position = padNewPosition;
+    }
+}
